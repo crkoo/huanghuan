13e151b337f575b5296fc9819b31b1914b6cc795
adophper <hello@adophper.com>
2017-10-11 21:48:58 +0800

    彩66

---
+++ 20171011/conf/config.js
@@ -0,0 +1,10 @@
+var webAppConfig = {
+    staticPath: '/static/',
+    defaultSkin: 'blue'
+};
+
+var slideList = [
+	{ 'imgUrl': '../static/images/slide/m1.jpg'/*tpa=https://www.66cai66.com/static/images/slide/m1.jpg*/, "href": "" },
+	{ 'imgUrl': '../static/images/slide/m2.jpg'/*tpa=https://www.66cai66.com/static/images/slide/m2.jpg*/, "href": "" },
+    { 'imgUrl': '../static/images/slide/m3.jpg'/*tpa=https://www.66cai66.com/static/images/slide/m3.jpg*/, "href": "" }
+];
