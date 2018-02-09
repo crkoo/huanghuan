b1652c95b36664cb7ce43d278579df59ad9b3be2
adophper <hello@adophper.com>
2018-02-10 00:03:07 +0800

    弹窗修改

--- 20170915/update.sql
+++ 20170915/update.sql
@@ -9,3 +9,9 @@ ADD COLUMN `form_title2`  varchar(100) NULL COMMENT '表单二' AFTER `form_titl
 ALTER TABLE `dbl_content`
 MODIFY COLUMN `loss`  varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '亏损金额' AFTER `orderId`,
 ADD COLUMN `loss2`  varchar(100) NULL AFTER `loss`;
+
+
+ALTER TABLE `dbl_activity`
+ADD COLUMN `is_apply`  tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否需要申请优惠活动，0不需要，1需要' AFTER `form_title2`,
+ADD COLUMN `apply_number`  int(10) NOT NULL DEFAULT 0 COMMENT '可以申请次数，0表示不限制' AFTER `is_apply`;
+
